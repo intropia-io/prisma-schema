e83c175ded8f2389e6bab07eaf0213b800a3706c
BostonLeeK <bostonleek@gmail.com>
2022-11-10 14:49:58 +0200

    add title

---
+++ migrations/20221110124950_add_title_to_log/migration.sql
@@ -0,0 +1,21 @@
+/*
+  Warnings:
+
+  - Added the required column `title` to the `Logs` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Events" ALTER COLUMN "typeId" DROP NOT NULL,
+ALTER COLUMN "userId" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Logs" ADD COLUMN     "title" TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Organizations" ALTER COLUMN "typeId" DROP NOT NULL,
+ALTER COLUMN "userId" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Quests" ALTER COLUMN "typeId" DROP NOT NULL,
+ALTER COLUMN "dynastyId" DROP NOT NULL,
+ALTER COLUMN "userId" DROP NOT NULL;
