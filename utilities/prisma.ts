50df28c34be397c3cd60f7559981ad30351a68d4
Oleksii Sysoiev <bostonleek@gmail.com>
2023-01-22 17:45:33 +0200

    update all

--- utilities/prisma.ts
+++ utilities/prisma.ts
@@ -1,11 +1,11 @@
 import { PrismaClient } from '@prisma/client';
 
-let prisma;
+let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === "production") {
     prisma = new PrismaClient();
 } else {
-    let globalWithPrisma = global & {
+    let globalWithPrisma = global as typeof globalThis & {
         prisma: PrismaClient
     };
     if (!globalWithPrisma.prisma) {
